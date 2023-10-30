c6f0da81c2676efd31aee959eac174facdd97db1
sandy9542 <140615378+sandy9542@users.noreply.github.com>
2023-10-30 17:02:34 +0530

    new Api integrated (#5)
    Co-authored-by: SandeepFrost <sandeep@ace.on;ine>

--- app/src/main/java/com/send/retrofit/ApiInterface.java
+++ app/src/main/java/com/send/retrofit/ApiInterface.java
@@ -1,8 +1,10 @@
 package com.send.retrofit;
 
 import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ApiInterface {
 
--- app/src/main/java/com/send/retrofit/UserAdapter.java
+++ app/src/main/java/com/send/retrofit/UserAdapter.java
@@ -4,15 +4,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.userHolder> {
     MainActivity mainActivity;
     List<UserModel> userModelList;
-
 
     public UserAdapter(MainActivity mainActivity, List<UserModel> userModelList) {
         this.mainActivity = mainActivity;
