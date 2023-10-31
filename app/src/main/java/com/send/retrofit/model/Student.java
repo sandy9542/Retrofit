daf7ee16abd6b08867bae9ccdd885fd7ff432021
sandy9542 <140615378+sandy9542@users.noreply.github.com>
2023-10-31 18:08:48 +0530

    New api post (#8)
    * added post api also but not work I will do later
    
    * added post api also but not work I will do later
    
    ---------
    
    Co-authored-by: SandeepFrost <sandeep@ace.on;ine>

---
+++ app/src/main/java/com/send/retrofit/model/Student.java
@@ -0,0 +1,15 @@
+package com.send.retrofit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private String id;
+    private String name;
+    private String job;
+    private String createdAt;
+}
